872567f015fda9473615da203b57f9cdf95ceade
Ricardo Russ <ricardoruss@live.com>
2020-04-02 19:32:19 -0400

    added in html linking

--- server.js
+++ server.js
@@ -2,9 +2,26 @@ var express = require("express");
 
 var app = express();
 var PORT = process.env.PORT || 8080;
-
+var path = require("path");
+// var savedNotes = require("../db.json");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+
+// HTML Routes
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+  });
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+  });
+
+// API Routes
+
+app.get("/api/tables", function(req, res) {
+    res.json(savedNotes);
+  });
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
