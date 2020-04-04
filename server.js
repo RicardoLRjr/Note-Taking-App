a1eec29ff568eee53bf6ea32d8a903ff5b7ed6a0
Ricardo Russ <ricardoruss@live.com>
2020-04-03 22:12:15 -0400

    adding in function to write to notes

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var savedNotes = require("./db.json");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-
 // HTML Routes
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
@@ -15,8 +14,6 @@ app.get("/notes", function(req, res) {
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
   });
-
-
 // API Routes
 app.get("/api/notes", function(req, res) {
     fs.readFile(savedNotes, function(err, data){
@@ -27,62 +24,23 @@ app.get("/api/notes", function(req, res) {
         res.json(pastNotes)
 })
  });
-
-app.get("/api/db.json/:id", function(req, res) {
-    const index = parseInt(req.params.id);
-    console.log(index);
-  
-    if (isNaN(index)) {
-      res.status(400);
-      return res.send("there is not an id at this location");
-    }
-  
-    fs.readFile("db.json", function(err, data) {
-      if (err) {
-          console.log(err);
-        res.status(500);
-        return res.send("Something broke here...");
-      }
-      const pastNotes = JSON.parse(data);
-      if (index >= 0 && index < pastNotes.length) {
-        res.json(pastNotes[index]);
-        console.log("index function worked")
-      } else {
-        res.status(404);
-        return res.send("Error,there is no note here");
-      }
-    });
-  });
-
-
 app.post("/api/notes", function(req, res) {
-        var newNote = req.body; 
-        console.log(newNote);
-        fs.appendFile("db.json", newNote + '\n', function(err) {
-            if (err) {
-              console.log(err);
-            }
-            else {
-              console.log("Commit logged!");
-            }
-       res.json(newNote)   
-          })
+        var newNote2 = req.body; 
+        console.log(newNote2);
+        fs.readFile("db.json", function(err, data) {
+            const postNotes = JSON.parse(data);
+            postNotes.push(newNote2);
+            console.log(postNotes);
+            fs.writeFile("db.json", JSON.stringify(postNotes), function(err) {
+              if (err) {
+                res.status(500);
+                return res.send("the note failed to save..");
+              }
+              res.send("the note was written!");
+            });
+          });
           
   });
-//   * POST `/api/notes` - Should receive a new note to save on the request body,
-// add it to the `db.json` file, and then return the new note to the client.
-app.delete("/api/notes/:id", function(req, res){
-    fs.readFile(title)
-    // Insert corrected savedNotes section.
-})
-
-// * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. 
-// This means you'll need to find a way to give each note a unique `id` when it's saved. 
-// In order to delete a note, 
-// you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, 
-// and then rewrite the notes to the `db.json` file.
-
 app.listen(PORT, function() {
     console.log("http://localhost:" + PORT);
   });
-  
